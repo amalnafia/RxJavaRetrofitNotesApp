78dbbc65cce2628e670517396b3e90dcb9f6e5c9
oglitgit7 <amal.nafia@yahoo.com>
2019-10-28 17:47:06 +0300

    Initial commit

---
+++ app/src/main/java/com/example/rxjavaretrofitnotesapp/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.rxjavaretrofitnotesapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
